fde1a21f51977bca6ad6456b20bce9e0e7ee357c
xenesis <blazegaurav123@gmail.com>
2019-05-03 18:37:39 +0545

    Some Updates

--- src/App.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var React = require("react");
-function App() {
-    return React.createElement("h1", null, "Hi, Typescript-React-Parcel-Boilerplate");
-}
-exports["default"] = App;
