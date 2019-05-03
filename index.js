0f152aee057108bdd8384e71c0b6a231dd6f8005
xenesis <blazegaurav123@gmail.com>
2019-05-03 18:30:37 +0545

    Created Repo Added Configurations for Typescript, React and Packaging with Parcel

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var React = require("react");
+var react_dom_1 = require("react-dom");
+var App_1 = require("./src/App");
+react_dom_1["default"].render(React.createElement(App_1["default"], null), document.getElementById("app"));
---
+++ src/App.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var React = require("react");
+function App() {
+    return React.createElement("h1", null, "Hi, Typescript-React-Parcel-Boilerplate");
+}
+exports["default"] = App;
